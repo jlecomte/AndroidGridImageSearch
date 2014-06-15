2e76d919d3ccb09edefb3c69b8e8762a219a699e
Julien Lecomte <julien.lecomte@gmail.com>
2014-06-15 11:53:02 -0700

    Added infinite scroll, clear button and settings activity

--- src/net/julienlecomte/gridimagesearch/SearchActivity.java
+++ src/net/julienlecomte/gridimagesearch/SearchActivity.java
@@ -10,7 +10,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+// import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -28,6 +30,9 @@ public class SearchActivity extends Activity {
 	protected GridView gvResults;
 	ArrayList<ImageResult> results;
 	ImageResultArrayAdapter adapter;
+	protected String query = "";
+
+	private final int SETTINGS_ACTIVITY_REQUEST_CODE = 50; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,13 @@ public class SearchActivity extends Activity {
 		results = new ArrayList<ImageResult>();
 		adapter = new ImageResultArrayAdapter(this, results);
 		gvResults.setAdapter(adapter);
+
+		gvResults.setOnScrollListener(new EndlessScrollListener() {
+			@Override
+			public void onLoadMore(int page, int totalItemsCount) {
+				search(totalItemsCount);
+			}
+		});
 
 		gvResults.setOnItemClickListener(new OnItemClickListener() {
 			@Override
@@ -58,28 +70,67 @@ public class SearchActivity extends Activity {
 	}
 
 	public void onSearchButtonClick(View v) {
-		String query = etSearchBox.getText().toString();
-		Toast.makeText(this, "Searching for " + query,  Toast.LENGTH_SHORT).show();
+		String s = etSearchBox.getText().toString().trim();
 
+		if (s.equals(query)) {
+			return;
+		}
+
+		query = s;
+		adapter.clear();
+
+		if (query.length() > 0) {
+			Toast.makeText(this, "Searching for " + query,  Toast.LENGTH_SHORT).show();
+			search(0);
+		}
+	}
+
+	public void search(int offset) {
 		AsyncHttpClient http = new AsyncHttpClient();
 
 		http.get("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8" +
-					"&start=" + 0 +
-					"&q=" + Uri.encode(query),
+				"&start=" + offset +
+				"&q=" + Uri.encode(query),
 				new JsonHttpResponseHandler() {
 
-					@Override
-					public void onSuccess(JSONObject response) {
-						JSONArray jsonResults = null;
-						try {
-							jsonResults = response.getJSONObject("responseData").getJSONArray("results");
-							results.clear();
-							adapter.addAll(ImageResult.fromJSONArray(jsonResults));
-							Log.d("DEBUG", results.toString());
-						} catch (JSONException e) {
-							e.printStackTrace();
-						}
-					}
-				});
+			@Override
+			public void onSuccess(JSONObject response) {
+				JSONArray jsonResults = null;
+				try {
+					jsonResults = response.getJSONObject("responseData").getJSONArray("results");
+					adapter.addAll(ImageResult.fromJSONArray(jsonResults));
+					// Log.d("DEBUG", results.toString());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.ac_bar_menu, menu);
+        return true;
+    }
+
+	public void onSettings(MenuItem mi) {
+		Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+		Intent i = new Intent(this, SettingsActivity.class);
+		startActivityForResult(i, SETTINGS_ACTIVITY_REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == SETTINGS_ACTIVITY_REQUEST_CODE) {
+			// String result = data.getStringExtra("myvaluekey");
+			Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	public void onClear(View v) {
+		query = "";
+		etSearchBox.setText(query);
+		adapter.clear();
 	}
 }
